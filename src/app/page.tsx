3d97ad2557185a94f869f126641cfba9332ea418
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-12-25 18:08:48 +0530

    feat: some other changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,47 +9,70 @@ export default function Home() {
 	const [name, setName] = useState("");
 	const [linkedin, setLinkedin] = useState("");
 	const [email, setEmail] = useState("");
+
+	const handleDownload = () => {
+		const element = document.getElementById("download");
+		if (!element) return;
+	};
+
 	return (
-		<main className="flex flex-col-reverse md:flex-row max-w-7xl mx-auto px-7 gap-7">
-			<div className="flex justify-center flex-col gap-5 grow -translate-y-20 min-[450px]:translate-y-0">
-				<p className="text-2xl font-semibold">Enter your details below:</p>
-				<div className="flex justify-center items-center flex-col gap-2">
-					<input
-						type="text"
-						placeholder="Email"
-						value={email}
-						onChange={e => setEmail(e.target.value)}
-						className="p-2 border border-gray-300 rounded-md w-full"
-					/>
-					<input
-						type="text"
-						placeholder="Name"
-						value={name}
-						onChange={e => setName(e.target.value)}
-						className="p-2 border border-gray-300 rounded-md w-full"
-					/>
-					<input
-						type="text"
-						placeholder="Linkedin"
-						value={linkedin}
-						onChange={e => setLinkedin(e.target.value)}
-						className="p-2 border border-gray-300 rounded-md w-full"
+		<>
+			<main className="flex flex-col-reverse md:flex-row max-w-7xl mx-auto px-7 gap-7">
+				<div className="flex justify-center flex-col gap-5 grow -translate-y-20 min-[450px]:translate-y-0">
+					<p className="text-2xl font-semibold">
+						Enter your details below:
+					</p>
+					<div className="flex justify-center items-center flex-col gap-2">
+						<input
+							type="text"
+							placeholder="Email"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+							className="p-2 border border-gray-300 rounded-md w-full"
+						/>
+						<input
+							type="text"
+							placeholder="Name"
+							value={name}
+							onChange={e => setName(e.target.value)}
+							className="p-2 border border-gray-300 rounded-md w-full"
+						/>
+						<input
+							type="text"
+							placeholder="Linkedin"
+							value={linkedin}
+							onChange={e => setLinkedin(e.target.value)}
+							className="p-2 border border-gray-300 rounded-md w-full"
+						/>
+					</div>
+					<button
+						type="button"
+						onClick={handleDownload}
+						className="bg-[#2b28ff] hover:opacity-90 transition-opacity p-2 rounded-md text-white font-semibold"
+					>
+						Download
+					</button>
+				</div>
+				<div className="md:grow scale-[80%] -translate-x-[10%] min-[450px]:scale-[100%] min-[450px]:-translate-x-0 origin-top">
+					<IDCard
+						name={name}
+						linkedin={linkedin}
+						className="mx-auto"
 					/>
 				</div>
-				<button type="button" className="bg-[#2b28ff] hover:opacity-90 transition-opacity p-2 rounded-md text-white font-semibold">
-					Download
-				</button>
-			</div>
-			<div className="md:grow scale-[80%] -translate-x-[10%] min-[450px]:scale-[100%] min-[450px]:-translate-x-0 origin-top">
-				<IDCard name={name} linkedin={linkedin} className="mx-auto" />
-			</div>
-			{/* <div className="relative aspect-square overflow-hidden bg-red-100 rounded-2xl">
+			</main>
+			{/* Off-screen Div for Download */}
+			<div
+				className="aspect-square overflow-hidden rounded-2xl w-[760px] relative"
+				id="download"
+			>
 				<Image
 					src={bg}
 					alt="background gradient"
-					className="absolute top-0 left-0 w-full -z-10"
+					className="absolute top-0 left-0 w-full -z-10 h-full"
 				/>
-			</div> */}
-		</main>
+				<IDCard name={name} linkedin={linkedin} className="mx-auto" />
+			</div>
+		</>
 	);
 }
--- src/components/id-card.tsx
+++ src/components/id-card.tsx
@@ -4,95 +4,99 @@ import { ArrowUpRight, Slash } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-const IDCard = ({ name, linkedin, className }: { name: string; linkedin: string; className?: string; }) => {
-    return (
-        <div className={cn("w-[400px]", className)}>
-            {/* STRAP */}
-            <div className="w-[10%] h-[80px] bg-[#2b28ff] mx-auto shadow-xl" />
-            <div className="w-[48px] h-[30px] bg-[#171717] rounded-[4px] mx-auto" />
-            <div className="w-[24px] h-[18px] bg-[#171717] rounded-br-[4px] rounded-bl-[4px] mx-auto relative z-10" />
-            {/* STRAP */}
+const IDCard = ({
+	name,
+	linkedin,
+	className,
+}: { name: string; linkedin: string; className?: string }) => {
+	return (
+		<div className={cn("w-[400px]", className)}>
+			{/* STRAP */}
+			<div className="w-[10%] h-[80px] bg-[#2b28ff] mx-auto shadow-xl" />
+			<div className="w-[48px] h-[30px] bg-[#171717] rounded-[4px] mx-auto" />
+			<div className="w-[24px] h-[18px] bg-[#171717] rounded-br-[4px] rounded-bl-[4px] mx-auto relative z-10" />
+			{/* STRAP */}
 
-            {/* CARD */}
-            <div className="w-full bg-[#f6f6f6] border border-[#e9e9e9] mx-auto -my-[15px] px-1.5 relative z-0 rounded-[28px] shadow-2xl">
-                {/* CARD HOLD */}
-                <div className="w-[44px] h-[12px] my-[8px] bg-white rounded-full border border-[#e9e9e9] mx-auto" />
-                {/* CARD HOLD */}
+			{/* CARD */}
+			<div className="w-full bg-[#f6f6f6] border border-[#e9e9e9] mx-auto -my-[15px] px-1.5 relative z-0 rounded-[28px] shadow-2xl">
+				{/* CARD HOLD */}
+				<div className="w-[44px] h-[12px] my-[8px] bg-white rounded-full border border-[#e9e9e9] mx-auto" />
+				{/* CARD HOLD */}
 
-                {/* CARD INNER */}
-                <div className="bg-white min-h-[400px] rounded-[28px] px-[32px]">
-                    {/* THREE LINES */}
-                    <div className="flex gap-2 py-5">
-                        <div className="h-0.5 bg-[#2b28ff] w-full rounded-full" />
-                        <div className="h-0.5 bg-[#2b28ff] w-full rounded-full" />
-                        <div className="h-0.5 bg-[#2b28ff] w-full rounded-full" />
-                    </div>
-                    {/* THREE LINES */}
+				{/* CARD INNER */}
+				<div className="bg-white min-h-[400px] rounded-[28px] px-[32px]">
+					{/* THREE LINES */}
+					<div className="flex gap-2 py-5">
+						<div className="h-0.5 bg-[#2b28ff] w-full rounded-full" />
+						<div className="h-0.5 bg-[#2b28ff] w-full rounded-full" />
+						<div className="h-0.5 bg-[#2b28ff] w-full rounded-full" />
+					</div>
+					{/* THREE LINES */}
 
-                    {/* LOGO */}
-                    <div className="flex justify-between items-center py-5">
-                        <p className="font-mono font-bold text-xl leading-[1.1]">
-                            BiOS
-                            <br />
-                            Community
-                        </p>
-                        <Image
-                            src={biosLogo}
-                            alt="BiOS Community Logo"
-                            className="w-[72px]"
-                        />
-                    </div>
-                    {/* LOGO */}
+					{/* LOGO */}
+					<div className="flex justify-between items-center py-5">
+						<p className="font-mono font-bold text-xl leading-[1.1]">
+							BiOS
+							<br />
+							Community
+						</p>
+						<Image
+							src={biosLogo}
+							alt="BiOS Community Logo"
+							className="w-[72px]"
+						/>
+					</div>
+					{/* LOGO */}
 
-                    {/* TITLE */}
-                    <div className="py-7 space-y-2">
-                        <p className="font-semibold text-gray-600 text-lg">
-                            I'm attending
-                        </p>
-                        <h1 className="font-bold text-[50px] leading-[1.2]">
-                            Open Source Connect
-                        </h1>
-                    </div>
-                    {/* TITLE */}
+					{/* TITLE */}
+					<div className="py-7 space-y-2">
+						<p className="font-semibold text-gray-600 text-lg">
+							I'm attending
+						</p>
+						<h1 className="font-bold text-[50px] leading-[1.2]">
+							Open Source Connect
+						</h1>
+					</div>
+					{/* TITLE */}
 
-                    {/* DATE & VENUE */}
-                    <div className="py-6 flex justify-between items-center font-mono font-medium">
-                        <div className="font-mono font-medium flex gap-[3px] items-center">
-                            <span className="">12</span>
-                            <Slash className="-rotate-[20deg]" size={13} />
-                            <span className="">01</span>
-                            <Slash className="-rotate-[20deg]" size={13} />
-                            <span className="">2025</span>
-                        </div>
-                        <p className="">Solace Infotech</p>
-                    </div>
-                    {/* DATE & VENUE */}
+					{/* DATE & VENUE */}
+					<div className="py-6 flex justify-between items-center font-mono font-medium">
+						<div className="font-mono font-medium flex gap-[3px] items-center">
+							<span className="">12</span>
+							<Slash className="-rotate-[20deg]" size={13} />
+							<span className="">01</span>
+							<Slash className="-rotate-[20deg]" size={13} />
+							<span className="">2025</span>
+						</div>
+						<p className="">Solace Infotech</p>
+					</div>
+					{/* DATE & VENUE */}
 
-                    {/* NAME */}
-                    <div className="space-y-2 py-4">
-                        <p className="text-[18px] px-4 py-2 bg-[#2b28ff] font-semibold text-center rounded-full text-white">
-                            {name ? name : "Adarsh Dubey"}
-                        </p>
-                        <p className="text-center font-semibold text-gray-700">
-                            {linkedin
-                                ? linkedin
-                                : "linkedin.com/in/dubeyadarsh"}
-                        </p>
-                    </div>
-                    {/* NAME */}
-                </div>
-                {/* CARD INNER */}
+					{/* NAME */}
+					<div className="space-y-2 py-4">
+						<p className="text-[18px] px-4 py-2 bg-[#2b28ff] font-semibold text-center rounded-full text-white">
+							{name ? name : "Adarsh Dubey"}
+						</p>
+						<p className="text-center font-semibold text-gray-700">
+							{linkedin
+								? linkedin
+								: "linkedin.com/in/dubeyadarsh"}
+						</p>
+					</div>
+					{/* NAME */}
+				</div>
+				{/* CARD INNER */}
 
-                {/* WEBSITE LINK */}
-                <p className="py-2 text-sm text-gray-500 flex gap-1 items-center w-fit mx-auto">
-                    Create your own at{" "}
-                    <span className="text-black">bios.vercel.app/badge</span>
-                    <ArrowUpRight className="text-[#2b28ff]" size={18} />
-                </p>
-            </div>
-            {/* CARD */}
-        </div>
-    );
+				{/* WEBSITE LINK */}
+				<p className="py-2 text-sm text-gray-500 flex gap-1 items-center w-fit mx-auto">
+					Create your own at{" "}
+					<span className="text-black">bios.vercel.app/badge</span>
+					<ArrowUpRight className="text-[#2b28ff]" size={18} />
+				</p>
+			</div>
+			{/* CARD */}
+		</div>
+	);
 };
 
 export default IDCard;
